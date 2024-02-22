bca2b45748aa9de78af4c442e100b9fa17ef1e2e
MoAdel <madel7586@gmail.com>
2024-02-22 15:07:56 +0200

    Refactor search view layout

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -13,20 +13,23 @@ class SearchView extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Search a city'),
         ),
-        body: Center(
-          child: TextField(
-            onChanged: (query) {},
-            onSubmitted: (value) {},
-            autofillHints: const [AutofillHints.addressCity],
-            autocorrect: const bool.fromEnvironment('true'),
-            decoration: InputDecoration(
-              contentPadding:
-                  EdgeInsets.symmetric(vertical: 32, horizontal: 10.0),
-              suffixIcon: Icon(Icons.search),
-              labelText: 'Search',
-              border: OutlineInputBorder(),
-              hintFadeDuration: Duration(milliseconds: 100),
-              hintText: 'Enter city name',
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Center(
+            child: TextField(
+              onChanged: (query) {},
+              onSubmitted: (value) {},
+              autofillHints: const [AutofillHints.addressCity],
+              autocorrect: const bool.fromEnvironment('true'),
+              decoration: InputDecoration(
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 32, horizontal: 10.0),
+                suffixIcon: Icon(Icons.search),
+                labelText: 'Search',
+                border: OutlineInputBorder(),
+                hintFadeDuration: Duration(milliseconds: 100),
+                hintText: 'Enter city name',
+              ),
             ),
           ),
         ),
